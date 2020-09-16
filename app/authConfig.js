879bbcd2c0fa75df7bb1e1601014e641deae4ec3
Andrew Bredenkamp <bredenkamp@MacBook-Pro.local>
2020-09-16 15:54:17 +1200

    Adding ms files

---
+++ app/authConfig.js
@@ -0,0 +1,48 @@
+// Config object to be passed to Msal on creation.
+// For a full list of msal.js configuration parameters, 
+// visit https://azuread.github.io/microsoft-authentication-library-for-js/docs/msal/modules/_authenticationparameters_.html
+const msalConfig = {
+    auth: {
+        clientId: "8e3975a0-c07d-4222-9aa5-3075e5f9e385",
+        authority: "https://login.microsoftonline.com/common",
+        redirectUri: "http://localhost:3000/",
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback: (level, message, containsPii) => {
+                if (containsPii) {	
+                    return;	
+                }	
+                switch (level) {	
+                    case msal.LogLevel.Error:	
+                        console.error(message);	
+                        return;	
+                    case msal.LogLevel.Info:	
+                        console.info(message);	
+                        return;	
+                    case msal.LogLevel.Verbose:	
+                        console.debug(message);	
+                        return;	
+                    case msal.LogLevel.Warning:	
+                        console.warn(message);	
+                        return;	
+                }
+            }
+        }
+    }
+};
+
+// Add here the scopes that you would like the user to consent during sign-in
+const loginRequest = {
+    scopes: ["User.Read"]
+};
+
+// Add here the scopes to request when obtaining an access token for MS Graph API
+const tokenRequest = {
+    scopes: ["User.Read", "Mail.Read"],
+    forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
+};
