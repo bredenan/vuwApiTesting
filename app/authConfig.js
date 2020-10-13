d23b37be801231357ad7e9c84910a84652d0b2a9
Andrew Bredenkamp <bredenkamp@macbook-pro.staff.vuw.ac.nz>
2020-10-14 12:54:33 +1300

    Auto-commit (on Save).

--- app/authConfig.js
+++ app/authConfig.js
@@ -43,6 +43,6 @@ const loginRequest = {
 
 // Add here the scopes to request when obtaining an access token for MS Graph API
 const tokenRequest = {
-    scopes: ["User.Read", "Mail.Read", "Drive.Read", "Teams.Read", "Team.Read"],
+    scopes: ["User.Read", "Mail.Read", "Files.Read", "Files.Read.All", "Files.ReadWrite", "Files.ReadWrite.All", "Sites.Read.All", "Sites.ReadWrite.All"],
     forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
 };
